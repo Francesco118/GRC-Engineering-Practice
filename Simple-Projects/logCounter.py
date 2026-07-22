5e3a8ded24fd078f5a0e2055920f04e5da2375db
Francesco Castellano <johannes.francesco@outlook.com>
2026-07-21 21:03:17 -0600

    Just trying to commit

--- Simple-Projects/logCounter.py
+++ Simple-Projects/logCounter.py
@@ -1,5 +1,5 @@
 #READ
-with open ("log-sample.txt", "r") as log_file: #Open the log file in read mode (r)
+with open ("sample.txt", "r") as log_file: #Open the log file in read mode (r)
     log_contents = log_file.readlines() # Read the contents of the log file
     log_contents = [line.strip() for line in log_contents] #removes blank lines and whitespace from the beginning and end of each line
     for line in log_contents:
